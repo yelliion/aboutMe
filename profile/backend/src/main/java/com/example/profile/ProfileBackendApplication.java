ab3cc0f557c7bc90ad82652ee8fec2cea18296ea
yelliion <dohwang@ulogitech.com>
2025-02-19 23:05:53 +0900

    다시

---
+++ profile/backend/src/main/java/com/example/profile/ProfileBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileBackendApplication.class, args);
+	}
+
+}
---
+++ profile/frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="안뇽 상우야"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #34502c;
+  margin-top: 60px;
+}
+</style>
